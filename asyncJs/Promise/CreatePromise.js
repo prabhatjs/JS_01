9a3200b94ad56b3833833fc36ecae05d04653002
Prabhat Singh Yadav <prabhatmca18.jim@gmail.com>
2024-01-05 01:08:21 +0530

    how to handle promise chain and how to handle multiple promises

--- asyncJs/Promise/CreatePromise.js
+++ asyncJs/Promise/CreatePromise.js
@@ -1,16 +1,54 @@
 const Cart=["Apple","Banana","Orange"];
 
 let promise=CreateOrder(Cart);//it retrun promisr
-
+//add Promise chaining
 promise.then(function(orderID){ //consume a promise
     console.log(orderID);
+    return orderID;
+})
+.then(function(orderID){
+    return proceedToPay(orderID);
+})
+.then(function(payinfo){
+    console.log(payinfo)
+    return showOrderItems(payinfo);
+})
+.then(function(payinfo){
+    console.log(payinfo);
+    return AddCardDetail(payinfo);
+})
+.then(function(carddetails){
+    console.log(carddetails);
 })
 .catch(function(err){
     console.log("Promise Error Handle",err.message);
 })
 
-//produce a promise
-function CreateOrder(Cart){//this function is return a promise;
+
+function proceedToPay(orderID){
+    let paypromie=new Promise(function(resolve,reject){
+        resolve("Success",orderID);
+    })
+    return paypromie;
+}
+
+function showOrderItems(payinfo){
+    let prom=new Promise(function(resolve,reject){
+        resolve("Order Show");
+    })
+    return prom;
+}
+
+function AddCardDetail(carddetails){
+    let prom=new Promise(function(resolve,reject){
+        resolve("Card Number added 01919101");
+    })
+    return prom;
+}
+
+
+//produce a promise.//this function is return a promise;
+function CreateOrder(Cart){
     let mypromise=new Promise(function (resolve,reject){
         if(!validateCart(Cart)){
             const err=new Error("Cart is not Vallid");
@@ -25,5 +63,5 @@ function CreateOrder(Cart){//this function is return a promise;
 }
 function validateCart(Cart)
 {
-    return false;
+    return true;
 }
